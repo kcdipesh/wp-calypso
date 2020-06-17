7b1d18c6e05ef1857bbb61e6558dfedf22107796
undemian <undemian@gmail.com>
2020-06-17 14:02:43 +0300

    Remeber reader feed and sidebar selection

--- client/state/ui/reader/reducer.js
+++ client/state/ui/reader/reducer.js
@@ -7,6 +7,7 @@ import sidebar from './sidebar/reducer';
 import { combineReducers } from 'state/utils';
 import cardExpansions from './card-expansions/reducer';
 import hasUnseenPosts from './seen-posts/reducer';
+import { DESERIALIZE, SERIALIZE } from 'state/action-types';
 
 /*
  * Holds the last viewed stream for the purposes of keyboard navigation
@@ -14,9 +15,35 @@ import hasUnseenPosts from './seen-posts/reducer';
 export const currentStream = ( state = null, action ) =>
 	action && action.type === READER_VIEW_STREAM ? action.streamKey : state;
 
+/**
+ * Keep the last reader stream path selected by the user, for the purpose of autoselecting it
+ * when user navigates back to Reader
+ *
+ * @param state redux state
+ * @param action redux action
+ *
+ * @returns {null|string} last path selected
+ */
+export const lastPath = ( state = null, action ) => {
+	switch ( action.type ) {
+		case SERIALIZE:
+		case DESERIALIZE:
+			return state;
+
+		case READER_VIEW_STREAM:
+			if ( action.path && action.path.startsWith( '/read' ) ) {
+				return action.path;
+			}
+			break;
+	}
+	return state;
+};
+lastPath.hasCustomPersistence = true;
+
 export default combineReducers( {
 	sidebar,
 	cardExpansions,
+	lastPath,
 	currentStream,
 	hasUnseenPosts,
 } );
